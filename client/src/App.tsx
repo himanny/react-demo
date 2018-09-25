b47299a5d9b49c26ca328a9f7d4770a47e42f251
unknown <himannys@gmail.com>
2018-09-25 23:00:09 +0530

    basic start

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { createBrowserHistory} from 'history'
-import { Router, Route } from 'react-router';
+import { Router, Route, Link } from 'react-router-dom';
 
 import Customer from './components/Customers';
 import Order from './components/Orders';
@@ -17,12 +17,17 @@ export class App extends React.Component {
     return (
       <div>
       <p> Welcome to React </p>
-          <Products />
 <Router history={browserHistory}>
 <div>
+  <ul>
+    <li className ="topnav" > <Link to={'/'}> Outstanding Bills </Link> </li>
+    <li> <Link to={'/Customer'}> Payments </Link> </li>
+    <li> <Link to={'/Order'}> Rate Set Rollover </Link> </li>
+  </ul>
+  <hr />
           <Route path="/" component = {Products} />
-          <Route path="/Customer/" component = {Customer} />
-          <Route path="/Order/" component = {Order} />
+          <Route path="/Customer" component = {Customer}/> 
+          <Route path="/Order" component = {Order}/> 
           </div>
 </Router>
 
